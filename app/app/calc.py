cb487d80a45968ddfeb93ce7d31618610ef04d16
Stuart <stuart.jacobs@gmail.com>
2022-06-12 16:22:25 +1000

    updated test test

--- app/app/calc.py
+++ app/app/calc.py
@@ -6,3 +6,8 @@ Calculator functions
 def add(x, y):
     """and x & y and return result"""
     return x + y
+
+
+def subtract(x, y):
+    """subtract x from y and return result"""
+    return y - x
